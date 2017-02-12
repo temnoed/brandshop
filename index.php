d9e1d768d4a4ece2943b21bafb3a7080bf838bbf
temnoed <dc9257351633@gmail.com>
2017-02-12 22:35:38 +0300

    добавили вынос базы в отдельный файл.

--- index.php
+++ index.php
@@ -27,29 +27,9 @@
 <div id="content" >
 
     <?php
-    $goods = [
-        [
-            'id' => 1,
-            'name' => 'Iphone',
-            'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'img' => '/images/goods/iphone.jpg',
-            'price' => '2000 $'
-        ],
-        [
-            'id' => 2,
-            'name' => 'HTC',
-            'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'img' => '/images/goods/htc.jpg',
-            'price' => '1200 $'
-        ],
-        [
-            'id' => 3,
-            'name' => 'Samsung',
-            'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'img' => '/images/goods/samsung.jpg',
-            'price' => '1400 $'
-        ],
-    ];
+   
+
+    require('templates/base.php');  // подключаем базу данных
 
     $page = $_GET['page'];
 
