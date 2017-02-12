4b2b19ab7fc5b1b5fc5d0bcb6fc10f8974ff629f
temnoed <dc9257351633@gmail.com>
2017-02-12 23:43:56 +0300

    поправили относительный путь к файлу

--- templates/base.php
+++ templates/base.php
@@ -5,21 +5,21 @@ $goods = [
             'id' => 1,
             'name' => 'Iphone',
             'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'img' => '/images/goods/iphone.jpg',
+            'img' => 'images/goods/iphone.jpg',
             'price' => '2000 $'
         ],
         [
             'id' => 2,
             'name' => 'HTC',
             'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'img' => '/images/goods/htc.jpg',
+            'img' => 'images/goods/htc.jpg',
             'price' => '1200 $'
         ],
         [
             'id' => 3,
             'name' => 'Samsung',
             'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            'img' => '/images/goods/samsung.jpg',
+            'img' => 'images/goods/samsung.jpg',
             'price' => '1400 $'
         ],
     ];
